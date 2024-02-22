699347f2995ef0f4c4c3efb58f915f717eafd981
Brian Botha <brian.botha@matrix.ai>
2024-02-22 14:57:21 +1100

    docs: generating docs
    [ci skip]

---
+++ docs/assets/search.js
@@ -0,0 +1,1 @@
+window.searchData = JSON.parse("{\"rows\":[{\"kind\":32,\"name\":\"exec\",\"url\":\"variables/exec.html\",\"classes\":\"\"}],\"index\":{\"version\":\"2.3.9\",\"fields\":[\"name\",\"comment\"],\"fieldVectors\":[[\"name/0\",[0,2.877]],[\"comment/0\",[]]],\"invertedIndex\":[[\"exec\",{\"_index\":0,\"name\":{\"0\":{}},\"comment\":{}}]],\"pipeline\":[]}}");
